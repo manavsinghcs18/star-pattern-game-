a0450c0623a392347c5e9e83cb1dda99153bebfd
Manav Singh <manav.singh_cs18@gla.ac.in>
2021-02-28 11:03:43 +0530

    Initial commit

---
+++ star pattern.py
@@ -0,0 +1,98 @@
+
+num = int(input("Enter the number of rows: "))
+for i in range (num):
+    for j in range (num):
+        print("*",end="")
+    print()
+
+
+
+num=int(input("Enter the number: "))
+for i in range (1,num+1):
+    print("*"*i)
+
+
+num=int(input("Enter the number: "))
+for i in range (1,num+1):
+    print(" "*(num-i)+"*"*i)
+
+
+
+
+num=int(input("Enter the number: "))
+for i in range (1,num+1):
+    print(" "*(num-i)+"* "*i)
+
+
+
+
+num=int(input("Enter the number: "))
+for i in range (1,num+1):
+    print(" "*(num-i)+"* "*i)
+for i in range (num-1,0,-1):
+        print(" "*(num-i)+"* "*i)
+
+
+
+
+num=int(input("Enter the number: "))
+for i in range (num,0,-1):
+        print(" "*(num-i)+"* "*i)
+
+
+
+num=int(input("Enter the number: "))
+for i in range (num,0,-1):
+        print(" "*(num-i)+"*"*i)
+
+
+
+num=int(input("Enter the number: "))
+for i in range (num,0,-1):
+        print("*"*i)
+
+
+
+
+
+
+num=int(input("Enter the number: "))
+for i in range (1,num+1):
+    print("*"*i)
+for i in range (num-1,0,-1):
+        print("*"*i)
+
+
+
+
+
+
+num=int(input("Enter the number: "))
+for i in range (1,num+1):
+    print(" "*(num-i)+"*"*i)
+for i in range (num-1,0,-1):
+        print(" "*(num-i)+"*"*i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
